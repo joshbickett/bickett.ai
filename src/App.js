64cbb15fa959a3441b0595e6aca6803042bbe106
Josh Bickett <josh@bickett.net>
2022-08-11 00:59:33 -0700

    Add focus content

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from "./logo.svg";
 import "./App.css";
-import coding from "./assets/coding.png";
+import codingImg from "./assets/coding.png";
+import focusImg from "./assets/web.png";
 import styled from "@emotion/styled";
 
 function App() {
@@ -10,8 +11,8 @@ function App() {
         <div style={{ display: "grid", gridTemplateColumns: "2fr 1fr" }}>
           <div style={{ display: "flex", alignItems: "center" }}>
             <img
-              src={coding}
-              alt="tron"
+              src={codingImg}
+              alt="coding"
               style={{ width: "100px", borderRadius: "50%", margin: "10px" }}
             />
             <div
@@ -33,12 +34,34 @@ function App() {
             <ActiveNavButton
               style={{ backgroundColor: "#724CDB", borderRadius: "40%" }}
             >
-              current focus
+              my focus
             </ActiveNavButton>
             <NavButton>projects</NavButton>
             <NavButton>github</NavButton>
             <NavButton>writing</NavButton>
           </div>
+        </div>
+        <div
+          style={{
+            display: "grid",
+            gridTemplateColumns: "1fr 3fr 1fr 1fr",
+            margin: "30px 0",
+          }}
+        >
+          <div></div>
+
+          <div
+            style={{ display: "flex", alignItems: "center", fontSize: "30px" }}
+          >
+            Many industries have potential in the next decade, but what I am
+            most excited about is deep learning, AI, and software in general
+          </div>
+          <img
+            src={focusImg}
+            alt="focus"
+            style={{ width: "200px", borderRadius: "30px", margin: "10px" }}
+          />
+          <div></div>
         </div>
       </div>
     </div>
