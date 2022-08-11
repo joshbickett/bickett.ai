7fbce218533ae3abde8d6ba4a53cd72e4a4f1bf2
Josh Bickett <josh@bickett.net>
2022-08-10 23:52:44 -0700

    Add simple nav

--- src/App.js
+++ src/App.js
@@ -6,13 +6,7 @@ function App() {
   return (
     <div className="App">
       <div style={{ backgroundColor: "black", height: "100vh" }}>
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "space-between",
-            alignItems: "center",
-          }}
-        >
+        <div style={{ display: "grid", gridTemplateColumns: "2fr 1fr" }}>
           <div style={{ display: "flex", alignItems: "center" }}>
             <img
               src={coding}
@@ -21,14 +15,24 @@ function App() {
             />
             <div
               style={{
-                fontSize: "20px",
+                fontSize: "30px",
               }}
             >
               Josh Bickett
             </div>
           </div>
-
-          {/* <div>test</div> */}
+          <div
+            style={{
+              fontSize: "30px",
+              display: "flex",
+              justifyContent: "space-around",
+              alignItems: "center",
+            }}
+          >
+            <div>projects</div>
+            <div>github</div>
+            <div>writing</div>
+          </div>
         </div>
       </div>
     </div>
