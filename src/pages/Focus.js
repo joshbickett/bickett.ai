72ba7addac97a7b7ece8c761d220dc381cfc542b
Josh Bickett <josh@bickett.net>
2022-08-20 16:54:49 -0700

    Improvements to focus page

--- src/pages/Focus.js
+++ src/pages/Focus.js
@@ -1,6 +1,6 @@
 import "../App.css";
 
-import focusImg from "../assets/web.png";
+import scienceImg from "../assets/science.png";
 import writingImg from "../assets/writing.png";
 import startupImage from "../assets/startups.png";
 
@@ -16,36 +16,15 @@ export const Focus = () => {
             display: "flex",
             alignItems: "center",
             justifyContent: "center",
-            margin: "50px 100px",
+            margin: "50px 200px",
           }}
         >
           <div style={{ textAlign: "center", fontSize: "30px" }}>
-            Many industries have potential in the next decade, but what I am
-            most excited about is deep learning, AI, and software in general.
+            In the next decade, I am most excited about startups applying the
+            innovations in deep learning.
           </div>
         </div>
 
-        <div
-          style={{
-            display: "grid",
-            gridTemplateColumns: "1fr 1fr 2fr 1fr",
-          }}
-        >
-          <div></div>
-          <img
-            src={focusImg}
-            alt="focus"
-            style={{ width: "200px", borderRadius: "30px", margin: "10px" }}
-          />
-          <div>
-            <h3>Deep learning and AI</h3>
-            <p>
-              I am interested in the science and ensuring the technology created
-              are impactful in positive ways.
-            </p>
-          </div>
-          <div></div>
-        </div>
         <div
           style={{
             display: "grid",
@@ -75,6 +54,28 @@ export const Focus = () => {
         >
           <div></div>
           <img
+            src={scienceImg}
+            alt="focus"
+            style={{ width: "200px", borderRadius: "30px", margin: "10px" }}
+          />
+          <div>
+            <h3>Deep learning and AI</h3>
+            <p>
+              I am interested in the science and ensuring the technology created
+              are impactful in positive ways.
+            </p>
+          </div>
+          <div></div>
+        </div>
+
+        {/* <div
+          style={{
+            display: "grid",
+            gridTemplateColumns: "1fr 1fr 2fr 1fr",
+          }}
+        >
+          <div></div>
+          <img
             src={writingImg}
             alt="writing"
             style={{ width: "200px", borderRadius: "30px", margin: "10px" }}
@@ -86,7 +87,7 @@ export const Focus = () => {
             </p>
           </div>
           <div></div>
-        </div>
+        </div> */}
       </div>
     </div>
   );
