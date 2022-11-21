10a34c0ecf6aae440652de027b384f995c9e9968
Josh Bickett <josh@bickett.net>
2022-11-20 19:15:33 -0800

    Add redirect

--- src/App.js
+++ src/App.js
@@ -4,9 +4,21 @@ import { Writing } from "./pages/Writing";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import { useMediaQuery } from "react-responsive";
+import { useEffect } from "react";
 
 export const App = () => {
   const isMobile = useMediaQuery({ query: `(max-width: 760px)` });
+
+  // if domain is joshbickett.com redirect to bickett.com
+
+  useEffect(() => {
+    if (
+      window.location.hostname === "joshbickett.com" ||
+      window.location.hostname === "www.joshbickett.com"
+    ) {
+      window.location.replace("https://bickett.com");
+    }
+  }, []);
   return (
     <BrowserRouter>
       <Routes>
