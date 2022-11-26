5c21920f2e9f4ca72a8064f687e3cdb4480cd7cc
Josh Bickett <josh@bickett.net>
2022-11-26 10:01:18 -0800

    Easter Egg :) and another focus

--- src/pages/Focus.js
+++ src/pages/Focus.js
@@ -1,7 +1,7 @@
 import "../App.css";
 
 import scienceImg from "../assets/science.png";
-import writingImg from "../assets/writing.png";
+import webImage from "../assets/web2.png";
 import startupImage from "../assets/startups.png";
 
 import { NavigationBar } from "../components/NavigationBar";
@@ -9,6 +9,11 @@ import { useMediaQuery } from "react-responsive";
 
 export const Focus = () => {
   const isMobile = useMediaQuery({ query: `(max-width: 760px)` });
+
+  // create a random for 1 in 1000
+  const notRandom = Math.floor(Math.random() * 1000);
+
+  console.log("random", notRandom);
 
   return (
     <div>
@@ -45,8 +50,8 @@ export const Focus = () => {
                   margin: "0 20px",
                 }}
               >
-                Hello, I am a self-taught software engineer interested in deep
-                learning and its applications.
+                Hello, I am a self-taught software engineer trying to stay
+                focused.
               </div>
             </div>
 
@@ -66,17 +71,39 @@ export const Focus = () => {
               />
               <div>
                 <h3>Deep learning and AI</h3>
-                {/* <p>
-                  Before college, I was fascinated by physics and wondered what
-                  it would be like to be alive in 1905 and read of entirely new
-                  physical laws being discovered. As I studied the sciences in
-                  college, each seemed mature. When I discovered deep learning,
-                  it appeared to be a young and important science that would
-                  flourish in my lifetime.
-                </p> */}
+                {notRandom === 42 && (
+                  <p>
+                    <a href="https://www.youtube.com/watch?v=4cia_v4vxfE">
+                      You ain't seen nothing yet
+                    </a>
+                  </p>
+                )}
+                {notRandom !== 42 && <p>I am beginner, studying.</p>}
+              </div>
+            </div>
+            <div
+              style={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "flex-start",
+                margin: "0 40px",
+              }}
+              className="content-container"
+            >
+              <img
+                src={webImage}
+                alt="writing"
+                style={{
+                  maxWidth: "200px",
+                  borderRadius: "30px",
+                  margin: "10px",
+                }}
+              />
+              <div style={{ maxWidth: "600px" }}>
+                <h3>The Web</h3>
                 <p>
-                  I am interested in this young and important science that will
-                  flourish in our lifetimes.
+                  I am building web applications and chrome extensions with
+                  React.
                 </p>
               </div>
             </div>
@@ -101,8 +128,8 @@ export const Focus = () => {
               <div style={{ maxWidth: "600px" }}>
                 <h3>Startups</h3>
                 <p>
-                  I like working on small teams and I am interested in building
-                  useful tools and applying them at scale.
+                  I enjoy working on small teams. I am interested in building
+                  useful things that scale.
                 </p>
               </div>
             </div>
