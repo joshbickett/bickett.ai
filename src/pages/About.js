15fd59ed35782d7700ce8f204cc5905bfb1cdebf
Josh Bickett <josh@bickett.net>
2024-04-19 07:53:25 -0700

    that's all

--- src/pages/About.js
+++ src/pages/About.js
@@ -41,13 +41,13 @@ export const About = () => {
               }}
             >
               Hello, I am a software engineer at{" "}
-              <a href="https://www.hyperwriteai.com/">HyperWrite</a> building LLMs-based writing tools. In addition, we've explored automation of web tasks with an <a href="https://venturebeat.com/ai/hyperwrite-unveils-breakthrough-ai-agent-that-can-surf-the-web-like-a-human/">AI agent</a>. 
+              <a href="https://www.hyperwriteai.com/">HyperWrite</a> building LLM-based writing tools. In addition, we've explored the automation of web tasks with an <a href="https://venturebeat.com/ai/hyperwrite-unveils-breakthrough-ai-agent-that-can-surf-the-web-like-a-human/">AI agent</a>. 
               <br />
               <br />
-              On the side, I am currently explorer VLMs ability to <a href="https://venturebeat.com/ai/the-self-operating-computer-emerges/">operate computers</a> and <a href="https://www.yahoo.com/tech/youtuber-pits-chatgpt-against-super-104929439.html?guccounter=1&guce_referrer=aHR0cHM6Ly93d3cuZ29vZ2xlLmNvbS8&guce_referrer_sig=AQAAACbC4ujPehyZUC373tgmv7VuaQXtRqURVVNNVpBZ9s_YwmfWoZcbQCgLC3b4GQiqsoF9mlh6rNpeBDU1aN_ZnBv3VcRKnXlq4BOSC460gu9KjahXaSAxP45cbR8_VP8Yf2rwur5gaipOBg1LuvoLNVwIZx7K-T-q2x2_1eZ5aU5B">play games</a>. 
+              On the side, I am currently exploring VLMs' ability to <a href="https://venturebeat.com/ai/the-self-operating-computer-emerges/">operate computers</a> and <a href="https://www.yahoo.com/tech/youtuber-pits-chatgpt-against-super-104929439.html?guccounter=1&guce_referrer=aHR0cHM6Ly93d3cuZ29vZ2xlLmNvbS8&guce_referrer_sig=AQAAACbC4ujPehyZUC373tgmv7VuaQXtRqURVVNNVpBZ9s_YwmfWoZcbQCgLC3b4GQiqsoF9mlh6rNpeBDU1aN_ZnBv3VcRKnXlq4BOSC460gu9KjahXaSAxP45cbR8_VP8Yf2rwur5gaipOBg1LuvoLNVwIZx7K-T-q2x2_1eZ5aU5B">play games</a>. 
               <br />
               <br />
-              I am finishing up the <a href="https://www.coursera.org/specializations/deep-learning?utm_medium=sem&utm_source=gg&utm_campaign=B2C_NAMER_deep-learning_deeplearning-ai_FTCOF_specializations_country-US-country-CA&campaignid=904733485&adgroupid=148411448815&device=c&keyword=&matchtype=&network=g&devicemodel=&adposition=&creativeid=654837734383&hide_mobile_promo&gad_source=1&gclid=CjwKCAjwrIixBhBbEiwACEqDJStkVMyMnnK8stellg5b7pdrCHPCNjwlcL-zDA6YR2CVSgLo2rXPghoC6d8QAvD_BwE">Deep Learning Specialization</a> through <a href="https://www.deeplearning.ai/">DeepLearning.ai</a> and I am interesting in collaborating on research projects exploring VLM's ability to reason and play games.  
+              I am finishing up the <a href="https://www.coursera.org/specializations/deep-learning?utm_medium=sem&utm_source=gg&utm_campaign=B2C_NAMER_deep-learning_deeplearning-ai_FTCOF_specializations_country-US-country-CA&campaignid=904733485&adgroupid=148411448815&device=c&keyword=&matchtype=&network=g&devicemodel=&adposition=&creativeid=654837734383&hide_mobile_promo&gad_source=1&gclid=CjwKCAjwrIixBhBbEiwACEqDJStkVMyMnnK8stellg5b7pdrCHPCNjwlcL-zDA6YR2CVSgLo2rXPghoC6d8QAvD_BwE">Deep Learning Specialization</a> through <a href="https://www.deeplearning.ai/">DeepLearning.ai</a> and I am interested in collaborating on research projects exploring VLMs' ability to reason and play games.  
 
             </p>
           </div>
