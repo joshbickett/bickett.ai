05ee211888174a396d0dd17c15d882d488d060fd
Josh Bickett <josh@bickett.net>
2024-11-21 22:14:20 -0800

    Initial blog entry

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -31,8 +31,24 @@ export const Blog = ({ isMobile }) => {
         >
           <ContentContainer>
             <div>
-              <h3>A year since the launch of Self-Operating Computer</h3>
-              <p></p>
+              <h2>
+                A year since the launch of Self-Operating Computer Framework
+              </h2>
+              <p>
+                November 3rd, 2023 I pushed the “initial commit”. The goal of
+                the project? Create the first framework to enable multimodal
+                models to operate computers as humans, do with mouse and
+                keyboard.{" "}
+              </p>
+              <h4>The background</h4>
+              <p>
+                LLaVA-1.5 was released in October. Matt Shumer hosted the model
+                and shared me an endpoint. I started hacking around with it. Our
+                team had experimented with LLM and web agents for some time and
+                we were excited about the potential of multimodal agents. We
+                knew that GPT-4-vision-preview was on the horizon.
+              </p>
+              <p>...in progress</p>
             </div>
           </ContentContainer>
         </div>
