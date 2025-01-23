7d6124c12f10f805a1816b9769e68ca5f30573f2
Josh Bickett <josh@bickett.net>
2025-01-22 21:03:55 -0800

    iterate blog

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -30,11 +30,10 @@ export const Blog = ({ isMobile }) => {
               <h2>
                 A year since the launch of Self-Operating Computer Framework
               </h2>
-              <p>This blog post is a work in progress...incomplete</p>
               <p>
                 November 3rd, 2023 was the “initial commit”. The goal of the
                 project? Create the first framework to enable multimodal models
-                to operate computers as humans, do with mouse and keyboard.{" "}
+                to operate computers as humans, with mouse and keyboard.{" "}
               </p>
               <h3>The background</h3>
               <p>
@@ -42,87 +41,61 @@ export const Blog = ({ isMobile }) => {
                 <a href="https://x.com/sharifshameem/status/1405462642936799247">
                   Sharif's GPT-3 demo
                 </a>{" "}
-                in mid-2021. I was blown away. I thought, woah how is this going
-                to change the internet?
+                in mid-2021. I was blown away. It browsed the web, clicked
+                buttons, and purchased Airpods, all autonomously.
               </p>
+              {/* <p>
+                I was unaware of a long history of AI agents. I was unaware that
+                OpenAI created an computer-operating AI agent in 2016 called{" "}
+                <a href="">Universe</a> and "allows an AI agent to use a
+                computer like a human does: by looking at screen pixels and
+                operating a virtual keyboard and mouse."
+              </p> */}
+
               <p>
-                <a href="https://x.com/josh_bickett/status/1608544931668426753">
-                  I had a lot of questions{" "}
-                </a>
-                about what the future of the web looked like. There were many
-                ideas thrown around on Twitter. The idea of "generative-web"
-                came up and{" "}
-                <a href="https://x.com/josh_bickett/status/1609214028093575168">
-                  I took a shot at it myself
-                </a>{" "}
-                and{" "}
-                <a href="https://github.com/joshbickett/generative-web">
-                  open-source the project.
-                </a>{" "}
-                It is interesting to reflect on a few years later. I think
-                web-browsing agents have been less impactful in the last 2 years
-                than{" "}
-                <a href="https://x.com/josh_bickett/status/1608590282584645633">
-                  I expected.
-                </a>{" "}
-              </p>
-              <p>
-                I was greatly unaware of the long history of AI agents. I was
-                unaware that OpenAI created an computer-operating AI agent in
-                2016. The system was called <a href="">Universe</a> and "..
-                allows an AI agent to use a computer like a human does: by
-                looking at screen pixels and operating a virtual keyboard and
-                mouse."
+                Over a year later in September 2022, a similar web-browsing
+                agent project called{" "}
+                <a href="https://github.com/nat/natbot">Natbot</a> was
+                open-sourced and I got the chance to clone and try it. Nat threw
+                the project together in a weekend. He's a serious hacker.
               </p>
 
               <p>
-                The first open-source web-browsing AI agent I saw was{" "}
-                <a href="https://github.com/nat/natbot">Natbot</a> in September
-                2022. Nat threw this together in a weekend and it became clear
-                to me that he was a serious hacker.{" "}
-              </p>
-
-              <p>
-                After we saw Natbot, the HyperWrite team started building a
+                After Natbot, our team at HyperWrite started building a
                 web-browsing agent and after a few months of our heads down we
                 launched what I understand to be the{" "}
                 <a href="https://venturebeat.com/ai/hyperwrite-unveils-breakthrough-ai-agent-that-can-surf-the-web-like-a-human/">
                   first commercial available web-browsing agent.
                 </a>{" "}
-                It was built through{" "}
-                <a href="https://chromewebstore.google.com/detail/hyperwrite-ai-assistant/kljjoeapehcmaphfcjkmbhkinoaopdnd?hl=en-US">
-                  our Chrome Extension{" "}
-                </a>{" "}
-                that we had published originally with our TypeAhead feature
-                which univerally brough writing suggestions to any text field on
-                the web. We had a lot of fun building it and did many novel
-                things, but that's for another post.
               </p>
               <h3>A new type of AI agent</h3>
               <p>
-                While building the LLM-based web browsing agent, our team
-                taggled some challenges with the LLM-only approach on the web.
-                We anticipated multimodal agents that could iteract with
-                computers more similarly to humans would help alleviate some of
-                these challenges.{" "}
+                While building an web browsing agent, we came to understand the
+                limitations with LLM-based web browsing agent. LLMs can't
+                actually see the page. They see the HTML. Understanding a page
+                purely by through HTML context is challenging for LLMs. We
+                anticipated multimodal agents that could iteract with computers
+                more similarly to humans would help alleviate some of these
+                challenges.{" "}
               </p>
               <p>
                 Prior to GPT-4-vision-preview, Matt had shared an idea about
                 overlaying a grid on a screenshot and sending that to a
                 multimodal model so it could estimate pixels to click on a
-                screen. That key insight got me thinking...
+                screen.
               </p>
               <p>
                 LLaVA-1.5 was released in October 2023. Matt Shumer hosted the
                 model and shared an endpoint with me. I started hacking around
-                with it. We knew that GPT-4-vision-preview was on the horizon.
+                with it. We knew that GPT-4-vision-preview would arrive in the
+                near future.
               </p>
               <p>
-                I started hacking on what I decided to call a{" "}
+                At first I called the project the{" "}
                 <a href="https://x.com/josh_bickett/status/1716551556013924372">
-                  Generalist Computer Agent
+                  Generalist Computer Agent.
                 </a>{" "}
-                that would eventually become the{" "}
+                It would eventually become the{" "}
                 <a href="https://github.com/OthersideAI/self-operating-computer/">
                   Self-Operating Computer Framework.
                 </a>
@@ -131,13 +104,30 @@ export const Blog = ({ isMobile }) => {
                 I played with the idea of a visual OS-level agent for a week.
                 What became clear is that if you give a multimodal model an
                 objective, passed it screenshots, and prompt it to output mouse
-                and keyboard actions, it can operate a computer. I formulated
-                the following architecture and built it. As far as I'm aware it
-                didn't exist prior.
+                and keyboard actions, it could operate a computer. I formulated
+                an architecture and built it. As far as I'm aware it didn't
+                exist prior.
               </p>
               <b>Architecture</b>
               <ul>
-                <li>1. User passes objective</li>
+                <li>• User provides an objective </li>
+                <li>• Create a loop</li>
+                <li>
+                  <ul>
+                    <li>
+                      <li>
+                        • Passed the objective, a screenshot, and a prompt to a
+                        Vision-language Model (VLM)
+                      </li>
+                      <li>
+                        <ul>
+                          <li>• The prompt </li>
+                        </ul>
+                      </li>
+                    </li>
+                  </ul>
+                </li>
+
                 <li>2. AI takes a screenshot</li>
                 <li>
                   3. AI makes mouse or keyboard action toward the objective
@@ -145,6 +135,7 @@ export const Blog = ({ isMobile }) => {
                 <li>4. AI evaluates if it completed the objective</li>
                 <li>5. If not, go back to 2.</li>
               </ul>
+              <b>System Prompt</b>
               <p>
                 I tinkered and tinkered. I couldn't get it working with
                 Llava-1.5. Then...gpt-4-vision-preview was released. I hacked on
